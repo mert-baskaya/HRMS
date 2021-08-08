7adb4288fc3cd6232ca5e0ae0c31652791cbfe2c
Mert Başkaya <mertb1998@gmail.com>
2021-08-08 12:44:22 +0300

    Decleration of City, JobPosting and GlobalExceptionHandler

---
+++ src/main/java/kodlamaio/hrms/core/commons/GlobalExceptionHandler.java
@@ -0,0 +1,36 @@
+package kodlamaio.hrms.core.commons;
+
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        Map<String, Object> body = new HashMap<>();
+
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+
+        body.put("errors", errors);
+
+        return new ResponseEntity<>(body, headers, status);
+
+    }
+}
---
+++ src/main/java/kodlamaio/hrms/entities/concretes/City.java
@@ -0,0 +1,33 @@
+package kodlamaio.hrms.entities.concretes;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "cities")
+
+public class City {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "city_id")
+    private int id;
+
+    @NotNull(message = "Şehir ismi boş olamaz")
+    @Min(value = 2,message = "Şehir ismi en az 2 karakter olmalı")
+    private String cityName;
+
+    @OneToMany(mappedBy = "city")
+    private Set<JobPosting> jobPostings;
+
+}
