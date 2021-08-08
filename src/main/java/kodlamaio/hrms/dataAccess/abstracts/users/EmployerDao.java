daaff3f64ebe4b9cae16b3c652b4244aed3a492d
Mert Başkaya <mertb1998@gmail.com>
2021-08-08 20:15:32 +0300

    varius edits

--- src/main/java/kodlamaio/hrms/dataAccess/abstracts/users/EmployerDao.java
+++ src/main/java/kodlamaio/hrms/dataAccess/abstracts/users/EmployerDao.java
@@ -1,5 +1,6 @@
 package kodlamaio.hrms.dataAccess.abstracts.users;
 
+import kodlamaio.hrms.core.utilities.results.DataResult;
 import kodlamaio.hrms.entities.concretes.users.Employer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
